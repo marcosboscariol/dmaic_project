44e08d1e32fb57c2dd6505fd7ce78ce30bf10cfe
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-08 17:50:44 -0300

    fixed Mapa Mental

--- views/define.py
+++ views/define.py
@@ -389,7 +389,7 @@ with st.expander("__Mapa Mental__"):
     st.write("O Mapa Mental é utilizado para organizar as ideias e as pendências que surgem ao longo de cada etapa do projeto. Pode ser construído como a equipe quiser, desde que feito em grupo e que represente as informações de forma lógica.")
 
     imagem_mapa_mental = st.file_uploader(
-        "Carregue sua imagem do Mapa de Indicadores")
+        "Carregue sua imagem do Mapa Mental")
 
     if imagem_mapa_mental:
-        st.image(imagem_mapa_indicadores)
+        st.image(imagem_mapa_mental)
