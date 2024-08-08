5ebbd8e76e6834ce459f86305a5d76323585f5c4
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-08-08 15:09:36 -0300

    finished Mapa Indicadores

--- views/define.py
+++ views/define.py
@@ -286,3 +286,13 @@ with st.expander("__VOC/VOB__"):
                 st.write(f"__{value}__")
         else:
             st.write("Nenhum valor adicionado ainda.")
+
+# Mapa de Indicadores
+with st.expander("__Mapa de Indicadores__"):
+    st.write("A Árvore de Indicadores é utilizada para priorizar o escopo, partindo a partir da meta e desdobrando is itens conforme faça sentido para encontrar problemas menores mas que, quando resulvidos, solucionem um problema maior.")
+
+    imagem_mapa_indicadores = st.file_uploader(
+        "Carregue sua imagem do Mapa de Indicadores")
+
+    if imagem_mapa_indicadores:
+        st.image(imagem_mapa_indicadores)
